71863e7b5c885c04bdfcc40b88568b5669c23f14
Vijay Oommen <vijay.oommen@slalom.com>
2021-05-10 10:32:52 -0500

    added help text options

--- DBMigrator/Program.cs
+++ DBMigrator/Program.cs
@@ -12,14 +12,23 @@ namespace DBMigrator
         static void Main(string[] args)
         {
             var command = string.Join(' ', args);
-            command = "--list --runMigration";
-            parser = new Parser(settings => { settings.EnableDashDash = true; settings.CaseSensitive = false;  settings.AutoHelp = true; });
+            //command = "--list --runMigration";
+            parser = new Parser(settings => { 
+                settings.EnableDashDash = true; 
+                settings.CaseSensitive = false;  
+                settings.AutoHelp = true; 
+            });
 
             var result = parser.ParseArguments<AppOptions>(command.Split(' ')).WithParsed(RunWithOptions);
 
             result.WithNotParsed(errors =>
                 {
-                    var helptext = HelpText.AutoBuild(result, h => { return HelpText.DefaultParsingErrorsHandler(result, h); });
+                    var helptext = HelpText.AutoBuild(result, h => {
+                        h.AdditionalNewLineAfterOption = false;
+                        h.AddNewLineBetweenHelpSections = false;
+                        h.Heading = "DB Migration Utility";
+                        h.Copyright = "Western Alliance Bank - Slalom";
+                        return HelpText.DefaultParsingErrorsHandler(result, h); });
                     Console.WriteLine(helptext);
                 });
         }
@@ -39,7 +48,6 @@ namespace DBMigrator
             if (options.Rollback && options.Version > 0)
             {
                 MigrationRunner.MigrateDown(options.Version);
-                return;
             }
 
             if (options.RunMigration)
